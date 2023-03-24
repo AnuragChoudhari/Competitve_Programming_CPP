2e4ba62a02fe375ced6a505bbac912dc4a71e5bd
AnuragChoudhari <choudharianurag03@gmail.com>
2023-03-24 20:01:35 +0530

    sawpping

---
+++ swap_without_third_var.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int a = 5;
+    int b = 10;
+    
+    b = b - a;
+    a = a + b;
+    
+    cout << "a: " << a << endl << "b: " << b << endl;
+    
+}
