5c9ce441c25a423db775b48d703a1f60bbf7d837
AnuragChoudhari <choudharianurag03@gmail.com>
2023-03-31 09:15:10 +0530

    codechefarr2

---
+++ codechefarr2.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main(){
+    int t;
+    cin >> t;
+    while(t--){
+        int n;
+        cin >> n;
+        int arr[n];
+        for(int i=0;i<n;i++){
+            cin >> arr[i];
+        }
+        int count = 0;
+        for(int j=0;j<n;j++){
+           if(arr[j]>=1000){
+               count++;
+           }
+        }
+        cout << count << endl;
+    }
+}
